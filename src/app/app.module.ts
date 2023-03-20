9dd0b58107ca0bea40d09e004a5ce043737faba0
lai2-01 <lai2-01@itismeucci.com>
2023-03-20 12:41:17 +0100

    tabella fiha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BtnClickComponent } from './btn-click/btn-click.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabellaComponent } from './tabella/tabella.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BtnClickComponent
+    BtnClickComponent,
+    TabellaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
